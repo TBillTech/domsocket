36550fe9a60cfe0e5e0bed81c3ef8ef26b61aea1
Tom Henry <thirdheavenward@yahoo.com>
2015-07-04 14:29:52 -0600

    Refactor and simplify functions

--- grailsource/scripts/domsocket.js
+++ grailsource/scripts/domsocket.js
@@ -115,273 +115,264 @@ with(domsocket)
       return "Unknown stack";
   };
 
-  wsInfoAddListener = function(ws, theElement, eventName, theListener)
+  wsInfoAddListener = function(theElement, theListener)
   {
-    var wsInfo = wsInfoObjs[ws];
-    if(!(theElement.id in wsInfo.events))
-    {
-      wsInfo.events[theElement.id] = new Object();
-    }
-    var nodeEvents = wsInfo.events[theElement.id];
-    if(eventName in nodeEvents)
-      throw new Error(eventName + " is already defined for node " + theElement.id);
-    nodeEvents[eventName] = theListener;
-    theElement.addEventListener(theListener.eventName, theListener);
+      var nodeEvents = createNodeEvents(theListener.ws, theElement);
+      if(theListener.eventName in nodeEvents)
+	  throw new Error(theListener.eventName + " is already defined for node " + theElement.id);
+      nodeEvents[theListener.eventName] = theListener;
+      theElement.addEventListener(theListener.eventName, theListener);
   };
 
   wsInfoGetListener = function(ws, theElement, eventName)
   {
-    var wsInfo = wsInfoObjs[ws];
-    if(!(theElement.id in wsInfo.events))
-    {
-      throw new Error(theElement.id + " is not defined.");
-    }
-    var nodeEvents = wsInfo.events[theElement.id];
-    if(!(eventName in nodeEvents))
+      var nodeEvents = getNodeEvents(ws, theElement);
+      if(eventName in nodeEvents)
+	  return nodeEvents[eventName];
       throw new Error(eventName + " is not defined for node " + theElement.id);
-    return nodeEvents[eventName];
   };
 
   wsInfoRemoveListener = function(ws, theElement, eventName)
   {
-    var wsInfo = wsInfoObjs[ws];
-    if(!(theElement.id in wsInfo.events))
-    {
-      throw new Error(theElement.id + " is not defined.");
-    }
-    var nodeEvents = wsInfo.events[theElement.id];
-    if(eventName in nodeEvents)
-    {
-      var theListener = nodeEvents[eventName];
-      theElement.removeEventListener(theListener.eventName, theListener);
-      delete nodeEvents[eventName];
-    }
+      var nodeEvents = getNodeEvents(ws, theElement);
+      if(eventName in nodeEvents)
+      {
+	  var theListener = nodeEvents[eventName];
+	  theElement.removeEventListener(theListener.eventName, theListener);
+	  delete nodeEvents[eventName];
+      }
   };
 
   wsInfoRemoveAllListeners = function(ws)
   {
-    var wsInfo = wsInfoObjs[ws];
-    for(var theElementId in wsInfo.events)
-    {
-      var nodeEvents = wsInfo.events[theElementId];
-      for(var eventName in nodeEvents)
+      var wsInfo = wsInfoObjs[ws];
+      for(var theElementId in wsInfo.events)
       {
-        var theListener = nodeEvents[eventName];
-        theElement.removeEventListener(theListener);
+	  var nodeEvents = wsInfo.events[theElementId];
+	  for(var eventName in nodeEvents)
+	  {
+              var theListener = nodeEvents[eventName];
+              theElement.removeEventListener(theListener);
+	  }
       }
-    }
-    delete wsInfo.events;
+      delete wsInfo.events;
+  };
+
+  eventListenerIsMissing = function(theElement, wsInfo)
+  {
+      return !(theElement.id in wsInfo.events);
+  };
+
+  getNodeEvents = function(ws, theElement)
+  {
+      var wsInfo = wsInfoObjs[ws];
+      if(eventListenerIsMissing(theElement, wsInfo))
+	  throw new Error(theElement.id + " is not defined.");
+      return wsInfo.events[theElement.id];      
+  };
+
+  createNodeEvents = function(ws, theElement)
+  {
+      var wsInfo = wsInfoObjs[ws];
+      if(eventListenerIsMissing(theElement, wsInfo))
+	  wsInfo.events[theElement.id] = new Object();
+      return getNodeEvents(ws, theElement);
   };
 
   wsInfoAddElement = function(ws, theElement)
   {
-    var wsInfo = wsInfoObjs[ws];
-    if(!(theElement.id in wsInfo.nodeids))
-    {
-      wsInfo.nodeids[theElement.id] = theElement;
-    }
+      var wsInfo = wsInfoObjs[ws];
+      if(!(theElement.id in wsInfo.nodeids))
+	  wsInfo.nodeids[theElement.id] = theElement;
   };
 
   wsInfoRemoveElement = function(ws, theElement)
   {
-    var wsInfo = wsInfoObjs[ws];
-    if(!(theElement.id in wsInfo.nodeids))
-    {
-      throw new Error(theElement.id + " is not defined.");
-    }
-    delete wsInfo.nodeids[theElement.id];
+      var wsInfo = wsInfoObjs[ws];
+      if(!(theElement.id in wsInfo.nodeids))
+	  throw new Error(theElement.id + " is not defined.");
+      if(!(wsInfoOtherOwners(ws, theElement.id)))
+	 theElement.parentNode.removeChild(theElement);
+      delete wsInfo.nodeids[theElement.id];
   };
 
   wsInfoRemoveAllElements = function(ws)
   {
-    var wsInfo = wsInfoObjs[ws];
-    for(var theElementId in wsInfo.nodeids)
-    {
-      if(!(wsInfoOtherOwners(theElementId, ws)))
+      var wsInfo = wsInfoObjs[ws];
+      for(var theElementId in wsInfo.nodeids)
       {
-        var theElement = document.getElementById(msg.id);
-        theElement.parentNode.removeChild(theElement);
+	  if(!(wsInfoOtherOwners(ws, theElementId)))
+	  {
+              var theElement = document.getElementById(msg.id);
+              theElement.parentNode.removeChild(theElement);
+	  }
       }
-    }
-    delete wsInfo.nodeids;
+      delete wsInfo.nodeids;
   };
 
-  wsInfoOtherOwners = function(theElementId, notws)
+  wsInfoOtherOwners = function(thisws, theElementId)
   {
-    for(var ws in wsInfoObjs)
-    {
-      if(ws == notws)
-        continue;
-      alert(ws == notws);
-      if(theElementId in wsInfoObjs[ws].nodeids)
-        return true;
-    }
-    return false;
-  };
-
-  AppendChild = function(msg, ws)
-  {
-    if(msg.childTag !== "text")
-    {
-      var child = document.getElementById(msg.childId);
-      if(child === null)
-        child = document.createElement(msg.childTag);
-      else
-        child.tagName = msg.childTag;
-      child.id = msg.childId;
-      var parent = document.getElementById(msg.parentId);
-      if(child.parentNode === null)
+      for(var ws in wsInfoObjs)
       {
-        parent.appendChild(child);
+	  if(ws == thisws)
+              continue;
+	  if(theElementId in wsInfoObjs[ws].nodeids)
+              return true;
       }
-      else
-      {
-        if(child.parentNode !== parent)
-          throw new Error("child parentnode is wrong: parent(" + child.parentNode.id + ")!=msg(" + msg.parentId + ")");
-      }
-      wsInfoAddElement(ws, child);
-    }
-    else
-    {
-      var parent = document.getElementById(msg.parentId);
-      var child = document.createTextNode(msg.text);
-      parent.appendChild(child);
-    }
+      return false;
   };
 
   SetChild = function(msg, ws)
   {
     if(msg.childTag !== "text")
     {
-      RemoveChild(msg, ws);
-      InsertChild(msg, ws);
+	RemoveChild(msg, ws);
+	InsertChild(msg, ws);
     }
     else
-    {
-      var parent = document.getElementById(msg.parentId);
-      var child = parent.childNodes[msg.index];
-      child.nodeValue = msg.text;
-    }
+	SetTextNode(msg, ws);
   };
 
   InsertChild = function(msg, ws)
   {
     if(msg.childTag !== "text")
     {
+	var child = CreateChild(msg);
+	wsInfoAddElement(ws, child);
+    }
+    else
+	InsertTextNode(msg, ws);
+  };
+
+  CreateChild = function(msg)
+  {
       var child = document.getElementById(msg.childId);
       if(child === null)
         child = document.createElement(msg.childTag);
       else
         child.tagName = msg.childTag;
       child.id = msg.childId;
+      AdoptChild(msg, child);
+      return child;
+  };
+
+  AdoptChild = function(msg, child)
+  {
       var parent = document.getElementById(msg.parentId);
-      if(msg.index == parent.childNodes.length)
+      if((msg.index < 0) || (msg.index == parent.childNodes.length))
         parent.appendChild(child);
       else
         parent.insertBefore(child, parent.childNodes[msg.index])
-      wsInfoAddElement(ws, child);
-    }
-    else
-    {
-      var parent = document.getElementById(msg.parentId);
-      var child = document.createTextNode(msg.text);
-      if(msg.index == parent.childNodes.length)
-        parent.appendChild(child);
-      else
-        parent.insertBefore(child, parent.childNodes[msg.index])
-    }
   };
 
   RemoveChild = function(msg, ws)
   {
-    var parent = document.getElementById(msg.parentId);
-    var child = parent.childNodes[msg.index];
-    if(!(wsInfoOtherOwners(child.id, ws)))
-    {
+      var parent = document.getElementById(msg.parentId);
+      var child = parent.childNodes[msg.index];
       wsInfoRemoveElement(ws, child);
-      parent.removeChild(child);
-      child.id = "";
-    }
+  };
+
+  SetTextNode = function(msg, ws)
+  {
+      var parent = document.getElementById(msg.parentId);
+      var child = parent.childNodes[msg.index];
+      child.textContent = msg.text;
+  };
+
+  InsertTextNode = function(msg, ws)
+  {
+      var parent = document.getElementById(msg.parentId);
+      var child = document.createTextNode(msg.text);
+      if(msg.index == parent.childNodes.length)
+          parent.appendChild(child);
+      else
+          parent.insertBefore(child, parent.childNodes[msg.index])
   };
 
   SetAttribute = function(msg, ws)
   {
-    var theElement = document.getElementById(msg.id);
-    if(theElement.getAttribute(msg.name) instanceof Function)
-    {
-      theElement.getAttribute(msg.name)(msg.value);
-    }
-    else if(msg.name === "value")
-    {
-      theElement.value = msg.value;
-    }
-    else
-    {
-      theElement.setAttribute(msg.name, msg.value);
-    }
-    return msg.value;
+      var theElement = document.getElementById(msg.id);
+      if(theElement.getAttribute(msg.name) instanceof Function)
+	  theElement.getAttribute(msg.name)(msg.value);
+      else if(msg.name === "value")
+	  theElement.value = msg.value;
+      else
+	  theElement.setAttribute(msg.name, msg.value);
+      return msg.value;
   };
 
   GetAttribute = function(msg)
   {
-    var theElement = document.getElementById(msg.id);
-    if(theElement.getAttribute(msg.name) instanceof Function)
-    {
-      return theElement.getAttribute(msg.name)();
-    }
-    else if(msg.name === "value")
-    {
-      return theElement.value;
-    }
-    else
-    {
+      var theElement = document.getElementById(msg.id);
+      if(theElement.getAttribute(msg.name) instanceof Function)
+	  return theElement.getAttribute(msg.name)();
+      else if(msg.name === "value")
+          return theElement.value;
       return theElement.getAttribute(msg.name);
-    }
   };
 
   RemoveAttribute = function(msg, ws)
   {
-    var theElement = document.getElementById(msg.id);
-    theElement.removeAttribute(msg.name);
+      var theElement = document.getElementById(msg.id);
+      theElement.removeAttribute(msg.name);
   };
 
   HandleEvent = function(event)
   {
     var theListener = this;
-    var msg = new Object();
-    msg.type = "event";
-    msg.nodeid = theListener.nodeid;
-    msg.eventName = theListener.eventName;
-    if(theListener.hasOwnProperty("attributeArgs"))
-    {
+    var msg = CreateEventMessage(theListener);
+    theListener.ws.sendmsg(msg);
+  };
+
+  CreateEventMessage = function(theListener)
+  {
+      var msg = new Object();
+      msg.type = "event";
+      msg.nodeid = theListener.nodeid;
+      msg.eventName = theListener.eventName;
+      if(theListener.hasOwnProperty("attributeArgs"))
+	  msg.attributeArgs = GetAttributeResults(theListener);
+      return msg;
+  };
+
+  GetAttributeResults = function(theListener)
+  {
       var results = new Array();
-      for(var i = 0; i < theListener.attributeArgs.length; i++)
+      var attributeArgs = theListener.attributeArgs;
+      for(var i = 0; i < attributeArgs.length; i++)
       {
-        var attributeArg = theListener.attributeArgs[i];
-        var result = new Object();
-        result.id = attributeArg.id;
-        result.name = attributeArg.name;
-        result.value = GetAttribute(attributeArg);
-        results.push(result);
+	  var result = GetAttributeResult(attributeArgs[i]);
+	  results.push(result);
       }
-      msg.attributeArgs = results;
-    }
-    theListener.ws.send(JSON.stringify(msg)); theListener.ws.send("flush");
+      return results;
   };
  
+  GetAttributeResult = function(attributeArg)
+  {
+      var result = new Object();
+      result.id = attributeArg.id;
+      result.name = attributeArg.name;
+      result.value = GetAttribute(attributeArg);
+      return result;      
+  };
+
   AttachEvent = function(msg, ws)
   {
-    var theElement = document.getElementById(msg.id);
-    var theListener = new Object();
-    theListener.nodeid = msg.id;
-    theListener.eventName = msg.name;
-    if(msg.hasOwnProperty("attributeArgs"))
-    {
-      theListener.attributeArgs = msg.attributeArgs;
-    }
-    theListener.handleEvent = HandleEvent;
-    theListener.ws = ws;
-    wsInfoAddListener(ws, theElement, msg.name, theListener);
+      var theElement = document.getElementById(msg.id);
+      var theListener = CreateListener(msg, ws);
+      wsInfoAddListener(theElement, theListener);
+  };
+
+  CreateListener = function(msg, ws)
+  {
+      var theListener = new Object();
+      theListener.nodeid = msg.id;
+      theListener.eventName = msg.name;
+      if(msg.hasOwnProperty("attributeArgs"))
+	  theListener.attributeArgs = msg.attributeArgs;
+      theListener.handleEvent = HandleEvent;
+      theListener.ws = ws;
+      return theListener;
   };
 
   DetachEvent = function(msg, ws)
@@ -392,16 +383,12 @@ with(domsocket)
 
   UpdateEvent = function(msg, ws)
   {
-    var theElement = document.getElementById(msg.id);
-    var theListener = wsInfoGetListener(ws, theElement, msg.name);
-    if(msg.hasOwnProperty("attributeArgs"))
-    {
-      theListener.attributeArgs = msg.attributeArgs;
-    }
-    else if(theListener.hasOwnProperty("attributeArgs"))
-    {
-      theListener.removeAttribute("attributeArgs");
-    }
+      var theElement = document.getElementById(msg.id);
+      var theListener = wsInfoGetListener(ws, theElement, msg.name);
+      if(msg.hasOwnProperty("attributeArgs"))
+	  theListener.attributeArgs = msg.attributeArgs;
+      else 
+	  theListener.removeAttribute("attributeArgs");
   };
 
   DetachAllEvents = function(msg, ws)
@@ -409,10 +396,13 @@ with(domsocket)
     wsInfoRemoveAllListeners(ws);
   };
 
-  wsOnMessageHandlers.appendChild = AppendChild;
+  wsOnMessageHandlers.appendChild = InsertChild;
   wsOnMessageHandlers.setChild = SetChild;
   wsOnMessageHandlers.insertChild = InsertChild;
   wsOnMessageHandlers.removeChild = RemoveChild;
+  wsOnMessageHandlers.appendTextNode = InsertTextNode;
+  wsOnMessageHandlers.setTextNode = SetTextNode;
+  wsOnMessageHandlers.insertTextNode = InsertTextNode;
   wsOnMessageHandlers.setAttribute = SetAttribute;
   wsOnMessageHandlers.removeAttribute = RemoveAttribute;
   wsOnMessageHandlers.attachEvent = AttachEvent;
