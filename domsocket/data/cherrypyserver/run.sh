7ffb9a9791470c25eeb1f4942d61f4a7222ef028
Tom Henry <tom.henry@issaccorp.com>
2018-11-15 07:29:52 -0700

    Make the run.sh script work out of the box most of the time

--- domsocket/data/cherrypyserver/run.sh
+++ domsocket/data/cherrypyserver/run.sh
@@ -1,1 +1,1 @@
-sudo docker run -v $PWD/keys:/keys_volume:ro --net="host" tbilltechrep/cherrypyserver
+sudo docker run -v $PWD/keys:/keys_volume:ro --net="host" tbilltechrep/cherrypyserver ./serve.py --server_ip 0.0.0.0 --zmq_bind_ip "*" --server_port 8443 --zmq_port 5555
